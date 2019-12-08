74c79272537a18ff2fd0f6c5dc58dad061562ff9
NaTaShaRMN <tuananhnguyenyt@gmail.com>
2019-12-08 16:42:21 +0700

    create path and view page

--- app/ecommerce/urls.py
+++ app/ecommerce/urls.py
@@ -1,5 +1,31 @@
 from django.urls import path
-from .views import index,uploadfile
+from .views import (
+    about,
+    blog,
+    cart,
+    category,
+    checkout,
+    confirmation,
+    contact,
+    index,
+    elements,
+    login,
+    singleBlog,
+    singleProduct,
+    tracking,
+)
 urlpatterns = [
-    
+    path('about/',about,name='about'),
+    path('blog/',blog,name='blog'),
+    path('cart/',cart,name='cart'),
+    path('category/',category,name='category'),
+    path('checkout/',checkout,name='checkout'),
+    path('confirmation/',confirmation,name='confirmation'),
+    path('contact/',contact,name='contact'),
+    path('',index,name='index'),
+    path('elements/',elements,name='elements'),
+    path('login/',login,name='login'),
+    path('single-blog',singleBlog,name='singleblog'),
+    path('single-product/',singleProduct,name='single-product'),
+    path('tracking/',tracking,name='tracking')
 ]
--- app/ecommerce/views.py
+++ app/ecommerce/views.py
@@ -2,5 +2,41 @@ from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 # Create your views here.
 
+def about(request):
+    return render(request,'about.html')
 
+def blog(request):
+    return render(request,'blog.html')
 
+def cart(request):
+    return render(request,'cart.html')
+
+def category(request):
+    return render(request,'category.html')
+
+def checkout(request):
+    return render(request,'checkout.html')
+
+def confirmation(request):
+    return render(request,'confirmation.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def index(request):
+    return render(request,'index.html')
+
+def elements(request):
+    return render(request,'elements.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def singleBlog(request):
+    return render(request,'single-blog.html')
+
+def singleProduct(request):
+    return render(request,'single-product.html')
+
+def tracking(request):
+    return render(request,'tracking.html')
